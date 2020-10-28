20ab884e5cd0cc06267b2a4f42877b8e9ccc1201
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-28 17:23:13 -0400

    adding to workout.js and index.js

--- models/workout.js
+++ models/workout.js
@@ -2,25 +2,45 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: () => new Date()
-  },
-  exercises: [
+const workoutSchema = new Schema(
     {
-    type: {
-      type: String,
-      trim: true,
-      required: "Enter exercise name"
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter duration of exercise in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+            
+        ]
     },
-    duration: 
-    {
-        type:
-    }
-  ];
-});
 
-const Library = mongoose.model("Library", LibrarySchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = Library;
+module.exports = Workout;
