d8203588ba280939598de2583aa3473d7b5ec31a
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-29 17:17:06 -0400

    working on put, working on delete in api.js

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,8 @@ const Workout = require("../models/workout");
 
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
-        .then(dbWorkouts => {
-            res.json(dbWorkouts);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.json(err);
@@ -18,8 +18,8 @@ router.post("/api/workouts", (req, res) => {
 
 router.get("/api/workouts", (req, res) => {
     Workout.find()
-    .then(dbWorkouts => {
-        res.json(dbWorkouts);
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     })
     .catch(err => {
         res.json(err);
@@ -27,12 +27,29 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-    Workout.findByIdAndUpdate(
+    Workout.find(
+        params.id,
         {
-            
-        }
+            exercise: req.body.exercise
+        },
     )
-}
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({body}, res) => {
+    Workout.find({body})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 
 
