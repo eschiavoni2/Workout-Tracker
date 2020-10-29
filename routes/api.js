54f503f587880ddc81061377c18817ad7ca0e536
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-29 16:18:11 -0400

    added router get in api routes

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,31 @@
 const router = require("express").Router();
 const { response } = require("express");
+const { db } = require("../models/workout");
 const Workout = require("../models/workout");
 
 // no route for homepage
+
+
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
     })
     .catch(err => {
         res.json(err);
     });
 });
+
 
 
 module.exports = router;
