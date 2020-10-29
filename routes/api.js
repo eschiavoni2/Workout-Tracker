d4bd0517ad58143ee89d5372dd86e6c33c0e9cb9
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-29 10:19:12 -0400

    added create in api

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
+const { response } = require("express");
 const Workout = require("../models/workout");
 
 // no route for homepage
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
-
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
 });
 
 
